1d442b17f632d97b2ac19a301520a86cf13269c1
Roberto Ferro <r.ferro@sikuro.it>
2022-08-18 17:28:29 +0200

    Improve translations

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,9 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 
-import Translate, {translate} from '@docusaurus/Translate';
+import Translate from '@docusaurus/Translate';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -20,7 +19,11 @@ function HomepageHeader() {
               Build an optimized microservices quickly, focus on what matter
             </Translate>
         </h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">{
+          <Translate id="homepage.subtitle">
+            Go-Web is a web framework for Golang that makes it simple to write fast, secure web applications without sacrificing flexibility, usability, or type safety.
+          </Translate>
+        }</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
